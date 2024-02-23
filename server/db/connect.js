141cc62d42a51deb18ac74f5f06cc3840a499fa8
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-02-23 17:49:09 +0100

    server connect to database

---
+++ server/db/connect.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 import express from "express";
-
 const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
+import "./db/connect.js";
+dotenv.config();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(3000, () => {
   console.log("Server running in port 3000 !");
