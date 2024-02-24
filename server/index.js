c048ab29f40018166ecd2cd9d5dc9d5f8569a1d8
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-02-24 12:57:26 +0100

    create test server route

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ const app = express();
 import cors from "cors";
 import dotenv from "dotenv";
 import "./db/connect.js";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//   })
+// );
 app.use(express.json());
+app.use("/server/user", userRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 app.listen(3000, () => {
   console.log("Server running in port 3000 !");
