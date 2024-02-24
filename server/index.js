7b0c1b23a636aa7f45aedf725ee5c629ac2808c7
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-02-24 21:06:55 +0100

    create signup server route

--- server/index.js
+++ server/index.js
@@ -4,19 +4,21 @@ import cors from "cors";
 import dotenv from "dotenv";
 import "./db/connect.js";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use("/server/user", userRouter);
+app.use("/server/auth", authRouter);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(3000, () => {
   console.log("Server running in port 3000 !");
