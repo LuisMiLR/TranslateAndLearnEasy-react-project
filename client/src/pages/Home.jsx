9b0acf5d5fd599650f983c3e85abf0c5b5aca520
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-03-07 23:09:13 +0100

    create complete  ui wavy-background

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { WavyBackground } from "../components/ui/wavy-background";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <WavyBackground
+        colors={["#1E34FA", "#FF5630", "#FF8731", "#1E34FA", "#FF5630"]}
+        className="max-w-4xl mx-auto pb-40"
+      >
+        <h1 className="text-2xl md:text-4xl lg:text-7xl text-slate-100 font-bold inter-var text-center">
+          Plongez dans la maitrise de l'Anglais
+        </h1>
+        <p className="text-base md:text-lg mt-4 text-slate-100 font-normal inter-var text-center">
+          L'apprentissage dynamique des mots dont vous avez besoin !
+        </p>
+      </WavyBackground>
+    </div>
+  );
 }
