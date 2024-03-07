f1bf5836529d0c258b2b0f2fb9bd0047517a55b1
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-03-07 19:13:11 +0100

    update ui navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-black-100 border-b border-gray-700 shadow-md  ">
-      <div className="flex justify-between items-center p-5">
+      <div className="flex justify-between items-center p-6">
         <Link to="/">
-          <h1 className="font-bolt text-sm sm:text-xl flex flex-wrap ml-5">
+          <h1 className="font-bolt text-base sm:text-2xl flex flex-wrap ml-5">
             <span className="text-slate-100">Translate& </span>
-            <span className="text-orange-400">LearnEasy</span>
+            <span className="text-orange-500">LearnEasy</span>
           </h1>
         </Link>
-        <ul className="text-slate-400 flex gap-5 mr-5">
+        <ul className="text-gray-400 flex gap-5 mr-5">
           <Link to="/">
             <li className="hidden sm:inline hover:text-slate-200">Home</li>
           </Link>
