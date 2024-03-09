59d8c48ee120e065ae653c4b41e1521ebdc878a0
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-03-09 16:25:06 +0100

    completesession 2 of homePage

--- client/src/components/ui/PulseButton.jsx
+++ client/src/components/ui/PulseButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PulseButton = ({ children }) => {
   return (
-    <button className="relative block w-32 h-12 uppercase text-center leading-16 text-white-1 border-none rounded-lg bg-purple50 cursor-pointer animate-pulse">
+    <button className="hover:bg-purple50 relative block w-32 h-12 uppercase text-center leading-16 text-white-1 border-none rounded-lg  bg-purple-400 cursor-pointer animate-pulse">
       {children}
     </button>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { WavyBackground } from "../components/ui/wavy-background";
 import imageTest2 from "../assets/images/imageTest2.png";
+import evolutionRang from "../assets/images/evolutionRang.png";
 import PulseButton from "../components/ui/PulseButton";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -20,9 +22,9 @@ export default function Home() {
         </WavyBackground>
       </section>
 
-      <section className="mb-10 text-slate-100 ">
+      <section className="text-slate-100 mb-7 ">
         <div className="flex flex-row bg-gradient-to-r from-purple100 to-blue-70 max-w-7xl mx-auto h-100 py-16 pl-16 rounded-lg">
-          <div className="flex flex-col items-start mr-10 pr-10   ">
+          <div className="flex flex-col items-start  mr-28">
             <h2 className="text-4xl font-semibold pb-8">
               Améliore tes compétences en Anglais
             </h2>
@@ -33,9 +35,8 @@ export default function Home() {
                   traducteur de mots Anglais - Français.
                 </p>
                 <p className="text-xl ">
-                  Il va te permettre de mémoriser les termes que tu cherches
-                  dans le traducteur grace à un systeme ingénieux
-                  d'apprentissage.
+                  Il te permet de mémoriser les termes que tu cherches dans le
+                  traducteur grace à un systeme ingénieux d'apprentissage.
                 </p>
               </div>
             </div>
@@ -45,15 +46,51 @@ export default function Home() {
                 d'aprentissage évolue rapidement
               </p>
             </div>
-            <div className="flex  justify-center pt-10">
-              <PulseButton>commencer</PulseButton>
-            </div>
+            <Link to="/Sign-up">
+              <div className="flex  justify-center pt-10 ">
+                <PulseButton>commencer</PulseButton>
+              </div>
+            </Link>
           </div>
           <div>
             <img
-              className="h-80 w-full object-cover  opacity-80 border-2 rounded-lg  border-gray-400 "
+              className=" h-80 w-full object-cover  opacity-80 border-2 rounded-lg  border-gray-500 "
               src={imageTest2}
               alt="dashboard"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="max-w-7xl mx-auto grid grid-cols-5 gap-x-7 text-slate-100 mb-7">
+        <div className="col-span-2 rounded-lg bg-dark py-16 px-16">
+          {/* Contenu du premier conteneur, 40% de largeur */}
+          <h2 className="text-4xl font-semibold pb-8">
+            Mémorise tout le nouveau vocabulaire
+          </h2>
+          <p className="text-xl">
+            De nombreuses études linguistiques recommandent de connaître entre
+            3000 et 5000 mots pour une compréhension courante de l'Anglais. Le
+            système dynamique de gamification va te permettre d'apprendre
+            durablement les nouveaux termes que tu recherches dans le
+            traducteur. Tu vas pouvoir t'entraîner et mesurer t'as courbe
+            d'apprentissage
+          </p>
+        </div>
+        <div className="col-span-3 rounded-lg bg-dark ">
+          {/* Contenu du deuxième conteneur, 60% de largeur */}
+          <h2 className="text-4xl font-semibold pb-8 py-16 px-16">
+            Gagne des rangs de progression et d'évolution
+          </h2>
+          <p className="text-xl pb-16 px-16">
+            Les défis d'apprentissages et de révisions classent tes performances
+            et ta progression, améliore ton profil et pousse tes compétences en
+            Anglais vers le nouveau niveau supérieur
+          </p>
+          <div className="col-span-4 w-full ">
+            <img
+              src={evolutionRang}
+              alt="rang de progression d'apprentissage coloré"
+              className="rounded-lg"
             />
           </div>
         </div>
